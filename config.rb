d021fe4ba29eb756351c8e9edb6006ff7eeccfa6
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-27 21:10:14 -0300

    curso de sass e compass finalizado

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ css_dir = "css"
 sass_dir = "css"
 images_dir = "imagens"
 javascripts_dir = "js"
+http_path="../"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -15,7 +16,7 @@ javascripts_dir = "js"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+ line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
